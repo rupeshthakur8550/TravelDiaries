59561aa3747fa02d00b2d5cd63fe5fa8040e611a
Rupesh Thakur <rupeshthakur80078@gmail.com>
2024-03-19 17:46:19 +0530

    Home Page Making

---
+++ client/src/components/Home/Benefits.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { MdAccessibilityNew, MdHail, MdLiquor, MdPrivateConnectivity } from 'react-icons/md';
+import benefit1 from '../../assets/images/SideBG.png';
+
+const Benefit = () => {
+    return (
+        <div className="container mx-auto px-4 mt-10 mb-10" >
+            <div className="flex flex-col md:flex-row items-center justify-center gap-12">
+                <div className="md:w-1/3 mb-8 md:mr-2">
+                    <img src={benefit1} alt="tour" className="w-full rounded-lg shadow-lg" />
+                </div>
+                <div className="md:w-1/2 text-center md:text-left gap-4">
+                    <div className="mb-8">
+                        <h2 className="text-3xl lg:text-4xl font-bold">Our Commitments</h2>
+                    </div>
+                    <p className="text-lg lg:text-xl mb-12 text-justify">
+                        Travel Diaries is dedicated to providing an enriching platform where users can preserve their travel memories and facilitate future adventures.
+                    </p>
+                    <div className="flex flex-wrap justify-between mr-2 ml-2">
+                        <div className="w-full sm:w-[45%] h-[150px] mb-12">
+                            <h4 className="flex items-center text-blue-600 font-semibold mb-4">
+                                <MdAccessibilityNew className="mr-2" />
+                                Own Pace
+                            </h4>
+                            <p className="text-justify">
+                                Travel Diaries empowers users to travel at their own pace. Whether it's exploring bustling cities or tranquil landscapes, users can plan their journeys according to their preferences and schedules.
+                            </p>
+                        </div>
+                        <div className="w-full sm:w-[45%] h-[150px] mb-12">
+                            <h4 className="flex items-center text-blue-600 font-semibold mb-4">
+                                <MdLiquor className="mr-2" />
+                                Complements
+                            </h4>
+                            <p className="text-justify">
+                                Enhance your travel experiences with Travel Diaries' curated suggestions and VIP Experiences. Discover unique adventures tailored to your tastes, making every journey unforgettable.
+                            </p>
+                        </div>
+                        <div className="w-full sm:w-[45%] h-[150px] mb-12">
+                            <h4 className="flex items-center text-blue-600 font-semibold mb-4">
+                                <MdPrivateConnectivity className="mr-2" />
+                                Secured
+                            </h4>
+                            <p className="text-justify">
+                                Your privacy and security are our top priorities at Travel Diaries. We employ robust measures to safeguard your personal information, ensuring a safe and confidential environment for all users.
+                            </p>
+                        </div>
+                        <div className="w-full sm:w-[45%] h-[150px] mb-12">
+                            <h4 className="flex items-center text-blue-600 font-semibold mb-4">
+                                <MdHail className="mr-2" />
+                                Guidance
+                            </h4>
+                            <p className="text-justify">
+                                Receive personalized guidance and insider tips from fellow travelers through Travel Diaries' chat section. Connect with like-minded individuals, share invaluable insights, and embark on transformative journeys together.
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default Benefit;
--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Footer from '../footer/Footer'
+import Banner from '../Home/Banner'
+import Benefit from '../Home/Benefits'
+import Services from '../Home/Services'
 
 function Home() {
   return (
     <>
-      <div className='min-h-screen'>Home</div>
+      <div className='min-h-screen'>
+      <Banner/>
+      <Benefit/>
+      <Services/>
+      </div>
     </>
 
   )
